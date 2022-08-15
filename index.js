b6b1a348f7cd4ba4ce9f2d6ea7bf239aa91385b8
Andrzej Rehmann <andrzej.rehmann@gmail.com>
2022-08-15 22:05:52 +0100

    Cache issues for 1min

--- index.js
+++ index.js
@@ -8,14 +8,18 @@ const jiraApiToken = alfy.userConfig.get('jiraApiKey')
 const credentials = btoa(`${jiraUser}:${jiraApiToken}`)
 const auth = { Authorization: `Basic ${credentials}` }
 const currentUserOpenTickets = `rest/api/3/search?jql=assignee=currentuser() AND status not in (Done,Closed)`
+const oneMinInMs = 60000
+
+const issuesToAlfyOutput = (body) =>
+  body.issues
+    .map((issue) => toAlfyOutputItem(issue, jiraUrl))
+    .reduce((prev, curr) => [...prev, curr], [])
 
 alfy
-  .fetch(`${jiraUrl}/${currentUserOpenTickets}`, { headers: auth })
-  .then((data) =>
-    alfy.output(
-      data.issues
-        .map((issue) => toAlfyOutputItem(issue, jiraUrl))
-        .reduce((prev, curr) => [...prev, curr], [])
-    )
-  )
-  .catch((err) => alfy.error(err))
+  .fetch(`${jiraUrl}/${currentUserOpenTickets}`, {
+    headers: auth,
+    maxAge: oneMinInMs,
+    transform: issuesToAlfyOutput,
+  })
+  .then(alfy.output)
+  .catch(alfy.error)
